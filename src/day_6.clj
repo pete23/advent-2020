344394d9ccfd8ad3a2f807f3794671a4fb224a3b
Pete Windle <git@pete23.com>
2020-12-06 11:52:24 +0000

    day 6

---
+++ src/day_6.clj
@@ -0,0 +1,25 @@
+(ns day-6
+  (:use clojure.test)
+  (:require [clojure.string :refer [split]]
+            [clojure.set :refer [union intersection]]))
+
+(def input (-> "resources/day-6.input"
+                slurp
+                (split #"\n\n")))
+
+(defn count-group [f s]
+  (count (apply f (map #(apply hash-set %) (split s #"\n")))))
+
+(defn part-1 [input]
+  (reduce + (map (partial count-group union) input)))
+
+(def test-input ["abc" "a\nb\nc" "ab\nac" "a\na\na\na" "b"])
+
+(deftest part-1-test
+  (is (= 11 (part-1 test-input))))
+
+(defn part-2 [input]
+  (reduce + (map (partial count-group intersection) input)))
+
+(deftest part-2-test
+  (is (= 6 (part-2 test-input))))
