83cfd9fe15ac5dd697c4e650ea944edc1f477fa5
Pete Windle <git@pete23.com>
2020-12-17 19:21:43 +0000

    refactor for simplicity and 7x performance; part-1-test from 250ms to 32ms, part-2-test from 6.33s to 0.95s

--- src/day_17.clj
+++ src/day_17.clj
@@ -1,5 +1,5 @@
 (ns day-17
-  (:require [clojure.string :refer [split split-lines replace]])
+  (:require [clojure.string :refer [split-lines]])
   (:use clojure.test)
   (:import [com.carrotsearch.hppc LongByteHashMap LongHashSet]
            [com.carrotsearch.hppc.procedures LongProcedure LongByteProcedure]))
@@ -7,32 +7,8 @@
 (def test-input [".#." "..#" "###"])
 (def input (-> "resources/day-17.input" slurp split-lines))
 
-;; hashes are live cells, dots are empty
-(def char->value {\# (byte 1) \. (byte 0)})
-
-;; coordinates encoded as (w+500) * 1000000000 (z+500) * 1000000, (y+500) * 1000, x
-(defn coord->key [[^long x ^long y ^long z ^long w]]
-  (let [z (or z 0) w (or w 0) y (or y 0) x (or x 0)]
-    (+ 500 x
-       (* 1000 (+ y 500))
-       (* 1000000 (+ z 500))
-       (* 1000000000 (+ w 500)))))
-
-(defn key->coord [^long k]
-  (let [x (- (rem k 1000) 500)
-        k' (quot k 1000)
-        y (- (rem k' 1000) 500)
-        k'' (quot k' 1000)
-        z (- (rem k'' 1000) 500)
-        w (- (quot k'' 1000) 500)]
-    [x y z w]))
-
-(def dimensional-perms [[-1 0 1] [-1000 0 1000] [-1000000 0 1000000] [-1000000000 0 1000000000]])
-
-(defn neighbours [^long dimensions ^long k]
-  (loop [n (vector k) ts (take dimensions dimensional-perms)]
-    (if (empty? ts) n
-        (recur (mapcat (fn [t] (map #(+ % t) n)) (first ts)) (rest ts)))))
+;; hashes are live cells, dots are empty cells
+(def char->value {\# true \. false})
 
 (defn input->coordinate-values [input]
   (apply concat
@@ -44,62 +20,76 @@
        l))
     input)))
 
-;; Interop to create a LongByteProcedure to sum the memory arena
-(defprotocol ValueRetriever
-  (get-value [this ^LongByteHashMap matrix]))
-
-(deftype ValueAdder [^{:unsynchronized-mutable true} ^long total]
-  LongByteProcedure
-  (^void apply [this ^long k ^byte v] (set! total (+ total v)))
-  ValueRetriever
-  (get-value [this memory] (set! total 0) (.forEach memory this) total))
-
-(deftype RelevantCells [^long dimensions ^LongHashSet relevant]
-  LongByteProcedure
-  (^void apply [this ^long k ^byte v]
-   (reduce #(.add relevant %2) relevant (neighbours dimensions k)))
-  ValueRetriever
-  (get-value [this matrix] (.forEach matrix this) relevant))
-
-(deftype Compute [^long dimensions ^LongByteHashMap old-matrix ^LongByteHashMap new-matrix]
-  LongProcedure
-  (^void apply [this ^long k]
-   (let [cell (.get old-matrix k)
-         sum (reduce + (map #(.get old-matrix %) (neighbours dimensions k)))]
-     (cond (and (= cell 1) (or (= sum 3) (= sum 4))) (.put new-matrix k 1)
-           (and (= cell 0) (or (= sum 3))) (.put new-matrix k 1)))))
-     
-(defn cycle [dimensions ^LongByteHashMap matrix]
-  (let [relevant-calculator (RelevantCells. dimensions (LongHashSet.))
-        ^LongHashSet relevant (.get-value relevant-calculator matrix)
-        _ (println "Considering" (.size relevant) "cells based on" (.size matrix) "live cells")
-        new-matrix (LongByteHashMap.)
-        computer (Compute. dimensions matrix new-matrix)
-        _ (.forEach relevant computer)]
-    new-matrix))
-
-(defn repeat-cycle [n dimensions matrix]
-  (if (= n 0) matrix
-      (recur (dec n) dimensions (cycle dimensions matrix))))
+;; each dimension is encoded +500 * 100^d
+;; HOWEVER this doesn't need to be multidimensional here as the only conversion
+;; done is from x,y into the encoding
+(defn coord->key [[^long x ^long y]]
+  (+ 500 x
+     (* 1000 (+ y 500))))
 
 (defn input->matrix [input]
   (let [matrix (LongByteHashMap.)]
     (->> input
          input->coordinate-values
-         (remove #(= (second %) 0))
+         (filter second)
          (map first)
          (map coord->key)
          (reduce #(.put matrix %2 (byte 1)) matrix))
     matrix))
 
+(defn neighbour-transforms [dimensions]
+  (let [magnitude (long (Math/pow 1000 (dec dimensions)))]
+    (if (= dimensions 0) [0]
+        (vec (mapcat (fn [direction]
+                       (map #(+ % (* direction magnitude))
+                            (neighbour-transforms (dec dimensions)))) [-1 0 1])))))
+
+(defn ->neighbour-fn [dimensions]
+  (let [v (neighbour-transforms dimensions)]
+    (fn [k] (mapv #(+ k %) v))))
+
+;; Interop to create a LongByteProcedure to dig in the matrix
+(defprotocol ValueRetriever
+  (get-value [this ^LongByteHashMap matrix]))
+
+(deftype RelevantCells [neighbour-fn ^LongHashSet relevant]
+  LongByteProcedure
+  (^void apply [this ^long k ^byte _]
+   (reduce #(.add relevant %2) relevant (neighbour-fn k)))
+  ValueRetriever
+  (get-value [this matrix] (.forEach matrix this) relevant))
+
+(deftype Compute [neighbour-fn ^LongByteHashMap old-matrix ^LongByteHashMap new-matrix]
+  LongProcedure
+  (^void apply [this ^long k]
+   (let [cell (.get old-matrix k)
+         sum (reduce + (map #(.get old-matrix %) (neighbour-fn k)))]
+     (when (or (= sum 3)
+               (and (= cell 1) (= sum 4)))
+       (.put new-matrix k 1)))))
+     
+(defn run-cycle [neighbour-fn ^LongByteHashMap matrix]
+  (let [relevant-calculator (RelevantCells. neighbour-fn (LongHashSet.))
+        ^LongHashSet relevant (.get-value relevant-calculator matrix)
+        ;_ (println "Considering" (.size relevant) "cells based on" (.size matrix) "live cells")
+        new-matrix (LongByteHashMap.)
+        computer (Compute. neighbour-fn matrix new-matrix)
+        _ (.forEach relevant computer)]
+    new-matrix))
+
+(defn repeat-cycle [neighbour-fn n matrix]
+  (if (= n 0) matrix
+      (recur neighbour-fn (dec n) (run-cycle neighbour-fn matrix))))
+
 (defn create-output [^LongByteHashMap matrix]
   (.size matrix))
 
 (defn evaluate [dimensions input cycles]
-  (->> input
-       input->matrix
-       (repeat-cycle cycles dimensions)
-       create-output))
+  (let [neighbour-fn (->neighbour-fn dimensions)]
+    (->> input
+         input->matrix
+         (repeat-cycle neighbour-fn cycles)
+         create-output)))
 
 (deftest part-1-test
   (is (= 112 (evaluate 3 test-input 6))
